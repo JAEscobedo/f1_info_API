62914ce05e7ccd517e6bdd15fc1327f67a0ac6a7
JAEscobedo <escobedoalex302@gmail.com>
2025-11-05 20:03:37 -0600

    Add Driver entity with JPA annotations and initial data for F1 2024 grid

---
+++ src/main/resources/data.sql
@@ -0,0 +1,40 @@
+-- F1 2024 Grid Data
+-- Red Bull Racing
+INSERT INTO driver (first_name, last_name, team, nationality, driver_number) VALUES ('Max', 'Verstappen', 'Red Bull Racing', 'Netherlands', 1);
+INSERT INTO driver (first_name, last_name, team, nationality, driver_number) VALUES ('Sergio', 'Pérez', 'Red Bull Racing', 'Mexico', 11);
+
+-- Ferrari
+INSERT INTO driver (first_name, last_name, team, nationality, driver_number) VALUES ('Charles', 'Leclerc', 'Ferrari', 'Monaco', 16);
+INSERT INTO driver (first_name, last_name, team, nationality, driver_number) VALUES ('Carlos', 'Sainz Jr.', 'Ferrari', 'Spain', 55);
+
+-- McLaren
+INSERT INTO driver (first_name, last_name, team, nationality, driver_number) VALUES ('Lando', 'Norris', 'McLaren', 'United Kingdom', 4);
+INSERT INTO driver (first_name, last_name, team, nationality, driver_number) VALUES ('Oscar', 'Piastri', 'McLaren', 'Australia', 81);
+
+-- Mercedes
+INSERT INTO driver (first_name, last_name, team, nationality, driver_number) VALUES ('Lewis', 'Hamilton', 'Mercedes', 'United Kingdom', 44);
+INSERT INTO driver (first_name, last_name, team, nationality, driver_number) VALUES ('George', 'Russell', 'Mercedes', 'United Kingdom', 63);
+
+-- Aston Martin
+INSERT INTO driver (first_name, last_name, team, nationality, driver_number) VALUES ('Fernando', 'Alonso', 'Aston Martin', 'Spain', 14);
+INSERT INTO driver (first_name, last_name, team, nationality, driver_number) VALUES ('Lance', 'Stroll', 'Aston Martin', 'Canada', 18);
+
+-- Alpine
+INSERT INTO driver (first_name, last_name, team, nationality, driver_number) VALUES ('Esteban', 'Ocon', 'Alpine', 'France', 31);
+INSERT INTO driver (first_name, last_name, team, nationality, driver_number) VALUES ('Pierre', 'Gasly', 'Alpine', 'France', 10);
+
+-- Williams
+INSERT INTO driver (first_name, last_name, team, nationality, driver_number) VALUES ('Alex', 'Albon', 'Williams', 'Thailand', 23);
+INSERT INTO driver (first_name, last_name, team, nationality, driver_number) VALUES ('Logan', 'Sargeant', 'Williams', 'United States', 2);
+
+-- AlphaTauri (now RB)
+INSERT INTO driver (first_name, last_name, team, nationality, driver_number) VALUES ('Yuki', 'Tsunoda', 'RB', 'Japan', 22);
+INSERT INTO driver (first_name, last_name, team, nationality, driver_number) VALUES ('Daniel', 'Ricciardo', 'RB', 'Australia', 3);
+
+-- Alfa Romeo (now Kick Sauber)
+INSERT INTO driver (first_name, last_name, team, nationality, driver_number) VALUES ('Valtteri', 'Bottas', 'Kick Sauber', 'Finland', 77);
+INSERT INTO driver (first_name, last_name, team, nationality, driver_number) VALUES ('Zhou', 'Guanyu', 'Kick Sauber', 'China', 24);
+
+-- Haas
+INSERT INTO driver (first_name, last_name, team, nationality, driver_number) VALUES ('Kevin', 'Magnussen', 'Haas', 'Denmark', 20);
+INSERT INTO driver (first_name, last_name, team, nationality, driver_number) VALUES ('Nico', 'Hülkenberg', 'Haas', 'Germany', 27);
